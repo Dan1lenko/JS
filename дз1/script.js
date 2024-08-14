64cb96f29dc300d23de788063e28a64cb69d1fd8
Dan1lenko <hdm070206@gmail.com>
2024-08-14 14:09:22 +0300

    1dz

---
+++ дз1/script.js
@@ -0,0 +1,15 @@
+let city = "Київ";
+city = "Львів";
+const address = city;
+alert(address);
+
+let n = 10;
+const result = Math.pow(n, 3);
+console.log(result);
+
+let pen = 4;
+let penPrice = 5.25;
+let pencil = 6;
+let pencilPrice = 3.5;
+const total = pen * penPrice + pencil * pencilPrice;
+document.write(`${total}`)
