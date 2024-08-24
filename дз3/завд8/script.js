443923e15e61ca25c2480acf0f8fa61df93914ad
Dan1lenko <hdm070206@gmail.com>
2024-08-24 22:59:57 +0300

    Update script.js

--- дз3/завд8/script.js
+++ дз3/завд8/script.js
@@ -2,14 +2,10 @@ let a = prompt("Введіть число: ");
 let b = prompt("Введіть степінь: ");
 let result = 1;
 
-if (b > 0) {
-  for (let i = 0; i < b; i++) {
-    result *= a;
-  }
-} else if (b < 0) {
-  for (let i = 0; i < b; i++) {
-    result *= a;
-  }
+for (let i = 0; i < Math.abs(b); i++) {
+  result *= a;
+}
+if (b < 0) {
   result = 1 / result;
 }
 alert(`${result}`);
